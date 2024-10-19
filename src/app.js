54f5892dc828057f84da3eebacf12986e385486b
tobiasr6 <requenatobias3@gmail.com>
2024-10-18 23:25:07 -0300

    change: Agregado de cors y ruta hacia productos

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors'); 
 const rubroRoutes = require('./routes/rubroRoutes');
+const productoRoutes = require('./routes/productoRoutes');
 // const userRoutes = require('./routes/userRoutes');
 
 // Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 // Rutas
+
+
 app.get('/', (req, res) => {
     res.send('Hola mundo!');
   });
 
   app.use('/api/rubro', rubroRoutes);
+  app.use('/api/producto', productoRoutes);
 
 // Manejo de errores
 app.use((err, req, res, next) => {
