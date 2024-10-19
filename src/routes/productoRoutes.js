0be08c7c41f7d6756b4753cf16c0c1dbb2bf0f58
tobiasr6 <requenatobias3@gmail.com>
2024-10-18 23:24:04 -0300

    feat[Productos]: Rutas o endpoints para acceder a los distintos metodos de los productos.

---
+++ src/routes/productoRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {
+    getAllProductos,
+    addProducto,
+    getProductoById,
+    editProducto,
+    deleteProducto,
+} = require('../controllers/productoController');
+
+const router = express.Router();
+
+// Rutas para rubros
+router.get('/', getAllProductos); // Obtener todos los rubros
+router.post('/', addProducto); // Agregar un nuevo rubro
+router.get('/:id', getProductoById); // Obtener un rubro por ID
+router.put('/:id', editProducto); // Editar un rubro
+router.delete('/:id', deleteProducto); // Eliminar un rubro
+
+module.exports = router;
